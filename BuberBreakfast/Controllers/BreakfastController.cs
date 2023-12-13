646b237a2d9dec8c61f4dac4431039b36ff509cd
Phat Nguyen <89678375+ngtphat-towa@users.noreply.github.com>
2023-12-13 17:47:14 +0700

    define Routes and Requests

---
+++ BuberBreakfast/Controllers/BreakfastController.cs
@@ -0,0 +1,35 @@
+using BuberBreakfast.Contracts.Breakfast;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuberBreakfast.Controllers;
+[ApiController]
+[Route("[controller]")]
+public class BreakfastController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult CreateBreakfast(CreateBreakfastRequest request)
+    {
+
+        return Ok(request);
+    }
+    [HttpGet("{id:guid}")]
+    public IActionResult GetBreakfast(Guid id)
+    {
+
+        return Ok(id);
+    }
+    [HttpPut("{id:guid}")]
+    public IActionResult UpdateBreakfast(Guid id, UpsertBreakfastRequest request)
+    {
+
+        return Ok(request);
+    }
+    [HttpDelete("{id:guid}")]
+    public IActionResult DeleteBreakfast(Guid id)
+    {
+
+        return Ok(id);
+    }
+}
+
+
